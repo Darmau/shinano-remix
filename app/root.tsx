9c21aba1a49f4b8636f6801b43d4249d3865067c
李大毛 <github@darmau.co>
2024-07-18 20:15:47 +0900

    update error

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import {
-  isRouteErrorResponse,
+  isRouteErrorResponse, Link,
   Links,
   Meta,
   Outlet,
@@ -7,7 +7,8 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
-  useRevalidator, useRouteError
+  useRevalidator,
+  useRouteError
 } from "@remix-run/react";
 import "./tailwind.css";
 import {json, LoaderFunctionArgs} from "@remix-run/cloudflare";
@@ -98,21 +99,40 @@ export function ErrorBoundary() {
 
   if (isRouteErrorResponse(error)) {
     return (
-        <div>
-          <h1>
-            {error.status} {error.statusText}
-          </h1>
-          <p>{error.data}</p>
-        </div>
-    );
+        <main className = "grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className = "text-center">
+            <p className = "text-base font-semibold text-indigo-600">{error.status}</p>
+            <h1 className = "mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{error.statusText}</h1>
+            <p className = "mt-6 text-base leading-7 text-gray-600">{error.data}</p>
+            <div className = "mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                  to="/"
+                  className = "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Home
+              </Link>
+            </div>
+          </div>
+        </main>
+  )
+    ;
   } else if (error instanceof Error) {
     return (
-        <div>
-          <h1>Error</h1>
-          <p>{error.message}</p>
-          <p>The stack trace is:</p>
-          <pre>{error.stack}</pre>
-        </div>
+        <main className = "grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className = "text-center">
+            <p className = "text-base font-semibold text-indigo-600">{error.name}</p>
+            <h1 className = "mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{error.message}</h1>
+            <p className = "mt-6 text-base leading-7 text-gray-600">{error.stack}</p>
+            <div className = "mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                  to = "/"
+                  className = "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Home
+              </Link>
+            </div>
+          </div>
+        </main>
     );
   } else {
     return <h1>Unknown Error</h1>;
