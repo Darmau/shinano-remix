e0555bed36bb43463fe39fdac11594103664b244
李大毛 <github@darmau.co>
2024-07-18 19:08:23 +0900

    feat: 注册功能

--- app/routes/$lang.login.tsx
+++ app/routes/$lang.login.tsx
@@ -22,9 +22,10 @@ export default function Login() {
       <main className = "bg-zinc-50">
         <div className = "flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
           <div className = "sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className = "mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              {label.title}
+            <h2 className = "mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-zinc-900">
+              {label.log_in_title}
             </h2>
+            <p className = "mt-6 text-center text-base text-zinc-500">{label.log_in_description}</p>
           </div>
 
           <div className = "mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
