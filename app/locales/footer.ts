aa7e9b75781622416539ce0536fb689617fdb008
李大毛 <github@darmau.co>
2024-07-23 16:23:24 +0900

    feat: footer调整

--- app/locales/footer.ts
+++ app/locales/footer.ts
@@ -1,38 +1,201 @@
 const FooterText = {
-  "zh": {
-    "blog": "博客",
-    "about": "本站",
-    "article": "文章",
-    "photography": "摄影",
-    "thought": "想法",
-    "contact": "联系",
-    "about_me": "关于作者",
-    "tech_stack": "技术栈",
-    "stats": "统计",
-  },
-
-  "en": {
-    "blog": "Blog",
-    "about": "About",
-    "article": "Articles",
-    "photography": "Photography",
-    "thought": "Thought",
-    "contact": "Contact",
-    "about_me": "About Me",
-    "tech_stack": "Tech Stack",
-    "stats": "Stats",
-  },
-
-  "jp": {
-    "blog": "ブログ",
-    "about": "について",
-    "article": "記事",
-    "photography": "写真",
-    "thought": "思考",
-    "contact": "連絡",
-    "about_me": "著者について",
-    "tech_stack": "技術スタック",
-    "stats": "統計",
-  },
+  "zh": [
+    {
+      "name": "文章",
+      "items": [
+        {
+          "name": "推荐文章",
+          "href": "/zh/articles/featured/1"
+        },
+        {
+          "name": "最新文章",
+          "href": "/zh/articles/all/1"
+        },
+        {
+          "name": "分类",
+          "href": "/zh/articles/categories"
+        },
+        {
+          "name": "归档",
+          "href": "/zh/articles/archive"
+        },
+      ]
+    },
+    {
+      "name": "摄影",
+      items: [
+        {
+          "name": "推荐摄影",
+          "href": "/zh/albums/featured/1"
+        },
+        {
+          "name": "全部摄影",
+          "href": "/zh/albums/all/1"
+        },
+      ]
+    },
+    {
+      "name": "想法",
+      items: [
+        {
+          "name": "所有想法",
+          "href": "/zh/thoughts/1"
+        },
+      ]
+    },
+    {
+      "name": "关于",
+      "items": [
+        {
+          "name": "作者",
+          "href": "/zh/about"
+        },
+        {
+          "name": "本站",
+          "href": "/zh/site"
+        },
+        {
+          "name": "联系我",
+          "href": "/zh/contact"
+        },
+        {
+          "name": "RSS",
+          "href": "/zh/rss"
+        },
+      ]
+    }
+  ],
+  "en": [
+    {
+      "name": "Article",
+      "items": [
+        {
+          "name": "Featured",
+          "href": "/en/articles/featured/1"
+        },
+        {
+          "name": "All",
+          "href": "/en/articles/all/1"
+        },
+        {
+          "name": "Categories",
+          "href": "/en/articles/categories"
+        },
+        {
+          "name": "Archive",
+          "href": "/en/articles/archive"
+        },
+      ]
+    },
+    {
+      "name": "Photography",
+      items: [
+        {
+          "name": "Featured",
+          "href": "/en/albums/featured/1"
+        },
+        {
+          "name": "All",
+          "href": "/en/albums/all/1"
+        },
+      ]
+    },
+    {
+      "name": "Thought",
+      items: [
+        {
+          "name": "All",
+          "href": "/en/thoughts/1"
+        },
+      ]
+    },
+    {
+      "name": "About",
+      "items": [
+        {
+          "name": "Author",
+          "href": "/en/about"
+        },
+        {
+          "name": "Site",
+          "href": "/en/site"
+        },
+        {
+          "name": "Contact",
+          "href": "/en/contact"
+        },
+        {
+          "name": "RSS",
+          "href": "/en/rss"
+        },
+      ]
+    }
+  ],
+  "jp": [
+    {
+      "name": "記事",
+      "items": [
+        {
+          "name": "おすすめ",
+          "href": "/jp/articles/featured/1"
+        },
+        {
+          "name": "すべて",
+          "href": "/jp/articles/all/1"
+        },
+        {
+          "name": "カテゴリ",
+          "href": "/jp/articles/categories"
+        },
+        {
+          "name": "アーカイブ",
+          "href": "/jp/articles/archive"
+        },
+      ]
+    },
+    {
+      "name": "写真",
+      items: [
+        {
+          "name": "おすすめ",
+          "href": "/jp/albums/featured/1"
+        },
+        {
+          "name": "すべて",
+          "href": "/jp/albums/all/1"
+        },
+      ]
+    },
+    {
+      "name": "考え",
+      items: [
+        {
+          "name": "すべて",
+          "href": "/jp/thoughts/1"
+        },
+      ]
+    },
+    {
+      "name": "について",
+      "items": [
+        {
+          "name": "著者",
+          "href": "/jp/about"
+        },
+        {
+          "name": "サイト",
+          "href": "/jp/site"
+        },
+        {
+          "name": "連絡先",
+          "href": "/jp/contact"
+        },
+        {
+          "name": "RSS",
+          "href": "/jp/rss"
+        },
+      ]
+    }
+  ]
 }
 export default FooterText;
