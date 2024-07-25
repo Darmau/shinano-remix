da1a786f11ade54cd8293b79dd0f7321fb07d8bf
李大毛 <github@darmau.co>
2024-07-25 21:12:39 +0900

    style: 全局loading

---
+++ app/components/PendingNavigation.tsx
@@ -0,0 +1,23 @@
+import { useNavigation } from "@remix-run/react";
+
+export  default function PendingNavigation() {
+  const navigation = useNavigation();
+  return navigation.state === "loading" ? (
+      <div className = "fixed inset-0 flex items-center justify-center z-50">
+        <div className = "animate-spin">
+          <svg
+              className = "h-12 w-12 text-violet-700" xmlns = "http://www.w3.org/2000/svg"
+              fill = "none" viewBox = "0 0 24 24"
+          >
+            <circle
+                className = "opacity-25" cx = "12" cy = "12" r = "10" stroke = "currentColor" strokeWidth = "4"
+            ></circle>
+            <path
+                className = "opacity-75" fill = "currentColor"
+                d = "M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+  ) : null;
+}
