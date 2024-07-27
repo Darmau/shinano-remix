fa4a6fdfbe7b57d6b4465f953764f9b5f69532a8
李大毛 <github@darmau.co>
2024-07-27 10:22:39 +0900

    perf: 优化不同图片分辨率的显示

--- app/components/ResponsiveImage.tsx
+++ app/components/ResponsiveImage.tsx
@@ -8,13 +8,23 @@ export default function ResponsiveImage({image, width, classList}: {image: Image
   const base = image.width > image.height ? 'width' : 'height';
 
   return (
-      <img
-          className = {classList}
-          src = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key}`}
-          srcSet = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key} 1x, ${prefix}/cdn-cgi/image/format=auto,${base}=${width * 2}/${image.storage_key} 2x`}
-          sizes="(max-width: 720px) 100vw, 2x"
-          alt = {image.alt || ''}
-          width = {width}
-      />
+      <picture>
+        <source
+            media = "(max-width: 639px)"
+            srcSet = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key}`}
+        />
+        <source
+            media = "(min-width: 640px)"
+            srcSet = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key} 1x, ${prefix}/cdn-cgi/image/format=auto,${base}=${width * 2}/${image.storage_key} 2x`}
+        />
+        <img
+            className = {classList}
+            src = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key}`}
+            srcSet = {`${prefix}/cdn-cgi/image/format=auto,${base}=${width}/${image.storage_key} 1x, ${prefix}/cdn-cgi/image/format=auto,${base}=${width * 2}/${image.storage_key} 2x`}
+            sizes = "(max-width: 720px) 100vw, 2x"
+            alt = {image.alt || ''}
+            width = {width}
+        />
+      </picture>
   )
 }
