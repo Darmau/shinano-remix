716e1e42aaa8886aaae67188ae6b6e306064b074
李大毛 <github@darmau.co>
2024-07-28 15:14:23 +0900

    style: 滚动效果优化

--- app/root.tsx
+++ app/root.tsx
@@ -108,7 +108,7 @@ export default function App() {
   const navigation = useNavigation();
 
   return (
-      <html lang = {lang}>
+      <html lang = {lang} className="scroll-smooth">
       <head>
         <meta charSet = "utf-8"/>
         <meta name = "viewport" content = "width=device-width, initial-scale=1"/>
