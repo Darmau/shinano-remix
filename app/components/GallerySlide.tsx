995d20f5794a21f34e3ef828d9dab2cbdcd9adaf
李大毛 <github@darmau.co>
2024-07-30 14:52:31 +0900

    style: 调整图片样式

--- app/components/GallerySlide.tsx
+++ app/components/GallerySlide.tsx
@@ -29,17 +29,18 @@ export default function GallerySlide({albumImages}: { albumImages: AlbumPhoto[] 
 
   return (
       <div>
-        <div>
+        <div className="relative">
           <Lightbox
             plugins={[Inline, Thumbnails]}
             inline={{
-              style: { width: "100%", maxWidth: "1280", aspectRatio: "3 / 2"}
+              style: { width: "100%", maxWidth: "1280", aspectRatio: "5 / 4"}
             }}
             slides={defaultSlides}
             styles={{
-              container: { backgroundColor: "white" },
-              thumbnailsContainer: { backgroundColor: "white" },
-              button: { filter: "none" }
+              container: { backgroundColor: "transparent" },
+              thumbnailsContainer: { backgroundColor: "transparent" },
+              button: { filter: "none" },
+              slide: { padding: 0, borderRadius: "12px" },
             }}
             thumbnails={{
               imageFit: 'contain',
@@ -49,19 +50,20 @@ export default function GallerySlide({albumImages}: { albumImages: AlbumPhoto[] 
               borderColor: '#52525b',
             }}
             render={{
-              iconPrev: () => <ChevronLeftIcon aria-hidden = "true" className = "h-5 w-5 text-black"/>,
-              iconNext: () => <ChevronRightIcon aria-hidden = "true" className = "h-5 w-5 text-black"/>
+              iconPrev: () => <div className="p-2 rounded-full bg-white/60 backdrop-blur-2xl">
+                <ChevronLeftIcon aria-hidden = "true" className = "h-5 w-5 text-black"/>
+              </div>,
+              iconNext: () => <div className="p-2 rounded-full bg-white/60 backdrop-blur-2xl">
+                <ChevronRightIcon aria-hidden = "true" className = "h-5 w-5 text-black"/>
+              </div>
             }}
           />
-        </div>
-        <div className="flex justify-center">
           <button
               onClick = {() => setOpen(true)}
               type = "button"
-              className = "inline-flex items-center gap-x-1.5 rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className = "absolute top-8 right-8 z-50 p-2 rounded-full bg-white/60 backdrop-blur-2xl"
           >
             <ArrowsPointingOutIcon aria-hidden = "true" className = "h-5 w-5"/>
-            Full Screen
           </button>
         </div>
         <Lightbox
@@ -96,7 +98,7 @@ function generateSlides(albumImages: AlbumPhoto[], prefix: string, targetWidth: 
         {
           src: `${prefix}/cdn-cgi/image/format=auto,width=1920/${photo.image.storage_key}`,
           width: 1440,
-          height: Math.floor(1440 * photo.image.height / photo.image.width)
+          height: Math.floor(1920 * photo.image.height / photo.image.width)
         }
       ]
     }
