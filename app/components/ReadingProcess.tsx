b180b7898cd3d44ecbdba68296d5c6ab52cad2b5
李大毛 <github@darmau.co>
2024-07-31 11:18:51 +0900

    fix: 给阅读进度条增加节流

--- app/components/ReadingProcess.tsx
+++ app/components/ReadingProcess.tsx
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
+import pkg from 'lodash';
+
+const {throttle} = pkg;
 
 function ScrollProgressBar() {
   const [scrollProgress, setScrollProgress] = useState(0);
 
   useEffect(() => {
-    const updateScrollProgress = () => {
+    const updateScrollProgress = throttle(() => {
       const scrollPx = document.documentElement.scrollTop;
       const winHeightPx =
           document.documentElement.scrollHeight -
           document.documentElement.clientHeight;
       const scrolled = (scrollPx / winHeightPx) * 100;
-
       setScrollProgress(scrolled);
-    };
+    }, 200);
 
     window.addEventListener('scroll', updateScrollProgress);
     updateScrollProgress(); // 初始化进度
@@ -24,7 +26,7 @@ function ScrollProgressBar() {
 
   return (
       <div
-          style={{
+          style = {{
             position: 'fixed',
             top: 0,
             left: 0,
@@ -32,7 +34,7 @@ function ScrollProgressBar() {
             height: '2px',
             backgroundColor: '#7c3aed',
             zIndex: 9999,
-            transition: 'width 0.2s ease-in-out',
+            transition: 'width 0.1s ease-in-out',
           }}
       />
   );
