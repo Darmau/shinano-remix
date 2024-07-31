b28dc2ce20504ed30be16bd720f566f1ecfe470e
李大毛 <github@darmau.co>
2024-07-31 20:19:25 +0900

    style: 取消想法里的大图浏览模式

--- app/routes/$lang.thought.$slug.tsx
+++ app/routes/$lang.thought.$slug.tsx
@@ -6,12 +6,16 @@ import {json, LoaderFunctionArgs} from "@remix-run/cloudflare";
 import {createClient} from "~/utils/supabase/server";
 import {Json} from "~/types/supabase";
 import ContentContainer from "~/components/ContentContainer";
+import ResponsiveImage from "~/components/ResponsiveImage";
+import {Image} from "~/types/Image";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/plugins/captions.css";
 
 export default function ThoughtDetail () {
   const { lang } = useOutletContext<{ lang: string }>();
   const label = getLanguageLabel(ThoughtText, lang);
 
-  const {thoughtData} = useLoaderData<typeof loader>();
+  const {thoughtData, thoughtImages} = useLoaderData<typeof loader>();
 
   const breadcrumbPages: BreadcrumbProps[] = [
     {
@@ -29,13 +33,33 @@ export default function ThoughtDetail () {
   return (
       <div className = "w-full max-w-6xl mx-auto p-4 md:py-8 mb-8 lg:mb-16">
         <Breadcrumb pages={breadcrumbPages} />
-        <ContentContainer content = {thoughtData.content_json as unknown as Json}/>
+        <div className = "grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className = "col-span-1 lg:col-span-2">
+            <ContentContainer content = {thoughtData.content_json as unknown as Json}/>
+            {thoughtImages && (
+                <div className = "space-y-2">
+                  {thoughtImages.map((image) => (
+                      <ResponsiveImage
+                          key = {image.image!.id} image = {image.image as Image} width = {560}
+                          classList = "rounded"
+                      />
+                  ))}
+                </div>
+            )}
+          </div>
+
+            <div className = "col-span-1 space-y-4">
+              留作评论
+            </div>
+        </div>
       </div>
-  )
+)
 }
 
 
-export async function loader({request, context, params}: LoaderFunctionArgs) {
+export async function loader({
+  request, context, params
+}: LoaderFunctionArgs) {
   const {supabase} = createClient(request, context);
   const slug = params.slug as string;
 
@@ -43,13 +67,11 @@ export async function loader({request, context, params}: LoaderFunctionArgs) {
   const {data: thoughtData} = await supabase
     .from('thought')
     .select(`
+      id,
       content_json,
       content_text,
       slug,
-      created_at,
-      thought_image (
-        image (id, alt, storage_key, width, height)
-      )
+      created_at
     `)
     .eq('slug', slug)
     .single();
@@ -61,7 +83,17 @@ export async function loader({request, context, params}: LoaderFunctionArgs) {
     })
   }
 
+  const {data: thoughtImages} = await supabase
+    .from('thought_image')
+    .select(`
+      order,
+      image (id, alt, storage_key, width, height, caption)
+    `)
+    .eq('thought_id', thoughtData.id)
+    .order('order', {ascending: true});
+
   return json({
-    thoughtData
+    thoughtData,
+    thoughtImages
   })
 }
