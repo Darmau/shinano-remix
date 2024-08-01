25c52231ee8779d1610914774a08a0e7487bc744
李大毛 <github@darmau.co>
2024-08-01 22:06:58 +0900

    feat: 摄影的评论

--- app/routes/$lang.album.$slug.tsx
+++ app/routes/$lang.album.$slug.tsx
@@ -1,6 +1,6 @@
-import {json, LoaderFunctionArgs} from "@remix-run/cloudflare";
+import {ActionFunctionArgs, json, LoaderFunctionArgs} from "@remix-run/cloudflare";
 import {createClient} from "~/utils/supabase/server";
-import {useLoaderData, useOutletContext} from "@remix-run/react";
+import {Link, useActionData, useLoaderData, useOutletContext} from "@remix-run/react";
 import {Json} from "~/types/supabase";
 import ContentContainer from "~/components/ContentContainer";
 import getDate from "~/utils/getDate";
@@ -11,14 +11,21 @@ import {MapPinIcon} from "@heroicons/react/20/solid";
 import Breadcrumb, {BreadcrumbProps} from "~/components/Breadcrumb";
 import getLanguageLabel from "~/utils/getLanguageLabel";
 import AlbumText from "~/locales/album";
+import CommentEditor from "~/components/CommentEditor";
+import {CommentBlock, CommentProps} from "~/components/CommentBlock";
 
-export default function AlbumDetail () {
-  const { lang } = useOutletContext<{ lang: string }>();
+export default function AlbumDetail() {
+  const {lang} = useOutletContext<{ lang: string }>();
   const {
     albumContent,
     albumImages,
-    MAPBOX
+    MAPBOX,
+    comments,
+    page,
+    limit,
+    totalPage
   } = useLoaderData<typeof loader>();
+  const actionResponse = useActionData<typeof action>();
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -38,9 +45,9 @@ export default function AlbumDetail () {
   ]
 
   return (
-      <div className="w-full max-w-8xl mx-auto p-4 md:py-8 lg:mb-16">
-        <Breadcrumb pages={breadcrumbPages} />
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className = "w-full max-w-8xl mx-auto p-4 md:py-8 lg:mb-16">
+        <Breadcrumb pages = {breadcrumbPages}/>
+        <div className = "grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className = "col-span-1 lg:col-span-2">
             <GallerySlide
                 albumImages = {albumImages as unknown as AlbumPhoto[]}
@@ -65,6 +72,36 @@ export default function AlbumDetail () {
             </div>
             <Mapbox mapboxToken = {MAPBOX} exifData = {albumImages![currentIndex].image!.exif as EXIF}/>
           </div>
+          <div className = "mt-8 col-span-1 lg:col-span-2">
+            <CommentEditor contentTable = {'to_photo'} contentId = {albumContent.id}/>
+            <div className = "flex flex-col gap-4 divide-y">
+              {actionResponse?.error && <p className = "error">{actionResponse.error}</p>}
+              {actionResponse?.comment && (
+                  <CommentBlock comment = {actionResponse.comment as unknown as CommentProps}/>
+              )}
+              {comments && comments.map((comment) => (
+                  <CommentBlock key = {comment.id} comment = {comment as unknown as CommentProps}/>
+              ))}
+            </div>
+            <div className = "py-8 flex justify-between">
+              {page > 1 && (
+                  <Link
+                      to = {{
+                        search: `?page=${page - 1}&limit=${limit}`
+                      }}
+                      className = "rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >{label.previous}</Link>
+              )}
+              {page < totalPage && (
+                  <Link
+                      to = {{
+                        search: `?page=${page + 1}&limit=${limit}`
+                      }}
+                      className = "ml-auto rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >{label.next}</Link>
+              )}
+            </div>
+          </div>
         </div>
       </div>
   )
@@ -74,6 +111,9 @@ export async function loader({request, context, params}: LoaderFunctionArgs) {
   const {supabase} = createClient(request, context);
   const lang = params.lang as string;
   const slug = params.slug as string;
+  const url = new URL(request.url);
+  const page = url.searchParams.get('page') ? parseInt(url.searchParams.get('page')!) : 1;
+  const limit = url.searchParams.get('limit') ? parseInt(url.searchParams.get('limit')!) : 20;
 
   // 摄影详情
   const {data: albumContent} = await supabase
@@ -109,9 +149,97 @@ export async function loader({request, context, params}: LoaderFunctionArgs) {
   .eq('photo_id', albumContent.id)
   .order('order', {ascending: true});
 
+  // 评论数据
+  const {data: comments} = await supabase
+  .from('comment')
+  .select(`
+      id,
+      user_id,
+      content_text,
+      created_at,
+      is_anonymous,
+      users (id, name)
+    `)
+  .eq('to_thought', albumContent.id)
+  .eq('is_blocked', false)
+  .eq('is_public', true)
+  .order('created_at', {ascending: false})
+  .range((page - 1) * limit, page * limit - 1);
+
+  // 评论总数
+  const {count} = await supabase
+  .from('comment')
+  .select('id', {count: 'exact'})
+  .eq('to_thought', albumContent.id)
+  .eq('is_blocked', false)
+  .eq('is_public', true);
+
+  // 总页数
+  const totalPage = count ? Math.ceil(count / limit) : 1;
+
   return json({
     albumContent,
     albumImages,
-    MAPBOX: context.cloudflare.env.MAPBOX_TOKEN
+    MAPBOX: context.cloudflare.env.MAPBOX_TOKEN,
+    comments,
+    page,
+    limit,
+    totalPage
   });
+}
+
+export async function action({request, context}: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const {supabase} = createClient(request, context);
+  const {data: {session}} = await supabase.auth.getSession();
+
+  if (!session) {
+    return json({
+      success: false,
+      error: 'Unauthorized',
+      comment: null
+    })
+  }
+
+  const {data: userProfile} = await supabase
+  .from('users')
+  .select('id, user_id')
+  .eq('user_id', session.user.id)
+  .single();
+
+  if (!userProfile) {
+    return json({
+      success: false,
+      error: 'User not exists',
+      comment: null
+    })
+  }
+
+  const content_text = formData.get('content_text') as string;
+  const to_photo = parseInt(formData.get('to_photo') as string);
+  const is_anonymous = formData.get('is_anonymous') === 'on';
+
+  const {data: newComment} = await supabase
+  .from('comment')
+  .insert({
+    user_id: userProfile.id,
+    content_text,
+    to_photo,
+    is_anonymous
+  })
+  .select(`
+      id,
+      user_id,
+      content_text,
+      created_at,
+      is_anonymous,
+      users (id, name)
+    `)
+  .single();
+
+  return json({
+    success: true,
+    error: null,
+    comment: newComment
+  })
 }
