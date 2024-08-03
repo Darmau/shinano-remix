62e56368001ca2aaaf72ddedcc4a0d6f55d9c1ab
李大毛 <github@darmau.co>
2024-08-03 15:09:11 +0900

    style: 正文样式

--- app/routes/$lang.article.$slug.tsx
+++ app/routes/$lang.article.$slug.tsx
@@ -58,44 +58,39 @@ export default function ArticleDetail() {
       <div className = "w-full max-w-6xl mx-auto p-4 md:py-8 mb-8 lg:mb-16">
         <ReadingProcess/>
         <Breadcrumb pages = {breadcrumbPages}/>
-        <div className = "grid grid-cols-1 md:grid-cols-3 gap-8">
-          <header className = "md:my-4 col-span-1 md:col-span-2 space-y-3 md:space-y-4">
-            <h1 className = "font-medium text-zinc-800 leading-normal text-4xl lg:text-5xl">{article.title}</h1>
-            <h2 className = "text-zinc-600 text-lg lg:text-xl">{article.subtitle}</h2>
-            {article.abstract &&
-                <p className = "p-4 rounded-md bg-zinc-100 text-zinc-600 leading-normal text-sm lg:text-base">{article.abstract}</p>}
-          </header>
-          <div className = "my-4 col-span-1 flex flex-col gap-6">
-            <div className = "space-y-2">
-              <h4 className = "text-sm text-violet-700 font-medium">{label.category}</h4>
-              <h3 className = "text-zinc-600 text-sm">{article.category!.title}</h3>
-            </div>
-            <div className = "space-y-2">
-              <h4 className = "text-sm text-violet-700 font-medium">{label.published_at}</h4>
-              <h3 className = "text-zinc-600 text-sm">{getDate(article.published_at!, lang)}</h3>
-            </div>
-            {article.topic && (
-                <div className = "space-y-2">
-                  <h4 className = "text-sm text-violet-700 font-medium">{label.topic}</h4>
+        <div className = "flex flex-col gap-8 md:gap-16">
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-4 gap-6 md:gap-8">
+            <header className = "space-y-4">
+              <h3 className = "text-sm text-violet-700 font-medium">{article.category!.title}</h3>
+              <h1 className = "font-medium text-zinc-800 leading-normal text-4xl lg:text-5xl">{article.title}</h1>
+              <h2 className = "text-zinc-600 text-lg lg:text-xl">{article.subtitle}</h2>
+              {article.abstract &&
+                  <p className = "p-4 rounded-md bg-zinc-100 text-zinc-600 leading-normal text-sm lg:text-base">
+                    {article.abstract}
+                  </p>}
+              <div className = "space-y-2">
+                <h3 className = "text-zinc-600 text-sm">{getDate(article.published_at!, lang)}</h3>
+              </div>
+              {article.topic && (
                   <ol className = "flex gap-2 flex-wrap">
                     {article.topic.map((topic: string, index: number) => (
                         <li key = {index} className = "text-sm text-zinc-600">#{topic}</li>
                     ))}
                   </ol>
-                </div>
-            )}
-          </div>
-          <div className = "col-span-1 space-y-4 md:space-y-8 md:col-span-3">
-            {article.cover && (
-                <div>
+              )}
+            </header>
+            <div className = "w-full">
+              {article.cover && (
                   <ResponsiveImage
                       image = {article.cover as unknown as Image} width = {960}
-                      classList = "w-full h-full rounded-md overflow-hiden object-cover aspect-[5/3]"
-                  />
-                </div>
-            )}
+                      classList = "w-full h-full rounded-md overflow-hiden object-cover"
+                />
+              )}
+            </div>
           </div>
-          <div className = "relative grid grid-cols-1 md:grid-cols-3 col-span-1 md:gap-24 md:col-span-3">
+
+          {/*正文*/}
+          <div className = "relative grid grid-cols-1 md:grid-cols-3 md:gap-24">
             <div className = "col-span-1 md:col-span-2 selection:bg-violet-800/60 selection:text-white">
               <ContentContainer content = {article.content_json as Json}/>
               <NextAndPrev
