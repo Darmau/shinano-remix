83aecf0859781454b049fbe4a4a521305177ea48
李大毛 <github@darmau.co>
2024-08-24 15:05:40 +0900

    add search button

--- app/routes/$lang.search.tsx
+++ app/routes/$lang.search.tsx
@@ -83,23 +83,23 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const query = url.searchParams.get("keyword") || "";
 
-  if (!query) {
-    return json({ results: null, query });
-  }
-
-  const MEILI_URL = context.cloudflare.env.MEILI_URL;
-  const MEILI_KEY = context.cloudflare.env.MEILI_KEY;
-
   // 从language表中获取语言数据，按照{1: 'zh', 2: 'en', 3: 'jp'}的格式返回
   const {supabase} = createClient(request, context);
   const {data: languages} = await supabase
-    .from('language')
-    .select('id, lang');
+  .from('language')
+  .select('id, lang');
 
   const langs = languages!.reduce((acc: Record<number, string>, lang) => {
     acc[lang.id] = lang.lang as string;
     return acc;
   }, {});
+
+  if (!query) {
+    return json({ results: null, query, langs });
+  }
+
+  const MEILI_URL = context.cloudflare.env.MEILI_URL;
+  const MEILI_KEY = context.cloudflare.env.MEILI_KEY;
 
   try {
     const data = await performSearch(query, MEILI_URL, MEILI_KEY);
@@ -110,7 +110,7 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
     });
   } catch (error) {
     console.error("搜索错误:", error);
-    return json({ results: null, error: "搜索过程中发生错误", query, langs });
+    return json({ results: null, error: "搜索过程中发生错误", query });
   }
 }
 
@@ -149,25 +149,25 @@ export default function Search() {
   };
 
   return (
-      <div className="w-full max-w-3xl mx-auto p-4 mb-8 lg:mb-16">
-        <h1 className="sr-only">{label.search}</h1>
-        <Form method="post" onSubmit={handleSubmit} className="flex w-full gap-4 my-8 lg:my-16">
+      <div className = "w-full max-w-3xl mx-auto p-4 mb-8 lg:mb-16">
+        <h1 className = "sr-only">{label.search}</h1>
+        <Form method = "post" onSubmit = {handleSubmit} className = "flex w-full gap-4 my-8 lg:my-16">
           <input
-              type="text"
-              name="keyword"
-              placeholder={label.placeholder}
-              defaultValue={loaderData.query}
-              className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+              type = "text"
+              name = "keyword"
+              placeholder = {label.placeholder}
+              defaultValue = {loaderData.query}
+              className = "min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
           />
           <button
-              type="submit"
-              className="flex-none rounded-md bg-violet-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
+              type = "submit"
+              className = "flex-none rounded-md bg-violet-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
           >
             {label.search}
           </button>
         </Form>
 
-        {results && <SearchResults results={results} langs={loaderData.langs} />}
+        {results && <SearchResults results = {results} langs = {loaderData.langs}/>}
       </div>
   );
 }
